bf9b681cc0c966c35a0cf8bfea4d282a918dd27e
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2023-09-11 16:19:35 -0400

    games-in-n-minutes addition/separation

--- games_countdown.bat
+++ games_countdown.bat
@@ -1,15 +1,20 @@
+::.bat
 @echo off
 
-::Used for prompting user for custom username input or hardcoded username
-:: set /p DISCORD_GAMES_COUNTDOWN_USERNAME="Enter your custom username: "
-:: set DISCORD_GAMES_COUNTDOWN_USERNAME="eric"
+:: Prompt the user for the number of minutes.
+set /p NUMBER_OF_MINUTES="Enter the number of minutes: "
+REM set /p DISCORD_GAMES_COUNTDOWN_USERNAME="Enter your custom username: "
 
 ::startup the flask app
 start python C:/_repos/chatforme_bots/games_countdown_bot.py
 
 ::Wait
-ping localhost -n 3 > nul
+ping localhost -n 5 > nul
 
-::Visit the app route to execute the games countdown command
-::curl -X POST -H "Content-Type: application/json" -d "{\"DISCORD_GAMES_COUNTDOWN_BOT_USERNAME\":\"%DISCORD_GAMES_COUNTDOWN_BOT_USERNAME%\"}" http://localhost:5000/send_games_command
-curl -X GET -H "Content-Type: application/json" http://localhost:5000/send_games_command
+:: Visit the app route to execute the games countdown command
+curl -X GET -H "Content-Type: application/json" http://localhost:5000/send_games_command?number_of_minutes=%NUMBER_OF_MINUTES%
+
+::Wait
+ping localhost -n 5 > nul
+
+exit
--- games_countdown_bot.py
+++ games_countdown_bot.py
@@ -1,3 +1,4 @@
+#.py
 """
 Discord-Flask Bot
 =================
@@ -50,6 +51,8 @@ Eric Hitchman, with the assistance of GPT4
 
 """
 from modules import load_yaml, load_env
+from my_modules.utils import shutdown_server
+
 import discord
 from flask import Flask, request, jsonify
 
@@ -58,7 +61,14 @@ import asyncio
 
 import logging
 import os
+import sys
+import time
 
+#############################
+#set yaml params
+yaml_filename = 'config-games-in.yaml'
+yaml_dirname = 'config'
+#############################
 
 # Initialize the app and client
 app = Flask(__name__)
@@ -69,28 +79,36 @@ client = discord.Client(intents=intents)
 logging.basicConfig(level=logging.INFO)
 
 # Load yaml file
-yaml_data = load_yaml(yaml_filename='config.yaml', yaml_dirname="C:\\_repos\\chatforme_bots\\config")
+yaml_data = load_yaml(yaml_filename=yaml_filename, yaml_dirname=yaml_dirname)
+env_filename = yaml_data['env_filename']
+env_dirname = yaml_data['env_dirname']
+
+# Load environment variables
+load_env(env_filename=env_filename, env_dirname=env_dirname)
+
+#Load params from env/config.yaml
 server_guild_id = int(yaml_data['server_guild_id']) #TODO: update to take from environment variable
 server_channel_id = int(yaml_data['server_channel_id']) #TODO: update to take from environment variable
 discord_games_countdown_username = yaml_data['discord_games_countdown_username']
-
-
-# Load environment variables
-load_env(env_filename=yaml_data['env_filename'], env_dirname=yaml_data['env_dirname'])
+discord_games_countdown_default_minutes = yaml_data['discord_games_countdown_default_minutes']
 DISCORD_BOT_TOKEN = os.getenv('DISCORD_BOT_PLAY_GAMES_IN_BOT_TOKEN')
 
+#########################
 # On ready
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
 
+#########################
 # Flask route (use app.route('/route',['POST']) iuw but include payload in .bat file or elsewhere )
 @app.route('/send_games_command')
-def send_games_command(number_of_minutes = 15):
+def send_games_command():
     # Use the below if you decide to grab user data from the payload sent  to /send_games_command
     # discord_games_countdown_username = request.json.get('DISCORD_GAMES_COUNTDOWN_USERNAME')
 
-    discord_games_countdown_username = yaml_data['discord_games_countdown_username']
+    # Retrieve the number_of_minutes from the query parameters. Use the default value if not provided.
+    number_of_minutes = request.args.get('number_of_minutes', default=discord_games_countdown_default_minutes, type=int)
+    print(f"DEBUG: Retrieved number_of_minutes: {number_of_minutes}")
 
     if not discord_games_countdown_username:
             return jsonify({'status':'failure', 'message':'You haven"t updated your custom username'})
@@ -99,21 +117,34 @@ def send_games_command(number_of_minutes = 15):
         channel = guild.get_channel(server_channel_id)
         if channel:
             asyncio.run_coroutine_threadsafe(channel.send(f'{discord_games_countdown_username} is @here and available in {str(number_of_minutes)} minutes for some games.  Hit "dem up!'), loop)
-            return jsonify({"status": "success", "message": "Command sent"})
+            jsonify({"status": "failure", "message": "Mission complete!"})            
+            
+            #Unsure if working
+            time.sleep(4)
+            shutdown_server()
+            sys.exit()
+            
+    #will only be reached if no guild/channel is set/found
     return jsonify({"status": "failure", "message": "Guild or channel not found"})
 
+
+
+#########################
 # Function to run Flask
 def run_flask():
     app.run(port=5000)
 
+#########################
 # Function to run the Discord client
 async def run_discord():
     await client.start(DISCORD_BOT_TOKEN)
 
+#########################
 # Create a new loop for discord
 loop = asyncio.new_event_loop()
 asyncio.set_event_loop(loop)
 
+#########################
 # Run Flask in its own thread
 flask_thread = threading.Thread(target=run_flask)
 flask_thread.start()

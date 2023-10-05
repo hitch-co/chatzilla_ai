d51ee6d86d9e647b4b78f62367507d241fc8cd1a
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2023-10-05 08:51:27 -0400

    removed prompts on bot start

--- twitch_bot_start_automsg.bat
+++ twitch_bot_start_automsg.bat
@@ -9,16 +9,16 @@ set prompt_list_automsg=videogames
 set prompt_list_chatforme=standard
 set input_port_number=3000
 
-:: Ask user to input param1
-set /p include_ouat=Would you like to run OnceUponATime bot (default:"%include_ouat%")?
-set /p include_automsg=Would you like to run AUTOMSG bot (default:"%include_automsg%")?
-set /p include_sound=Should the bots run with AUDIO (default:"%include_sound%")?:
+@REM :: Ask user to input param1
+@REM set /p include_ouat=Would you like to run OnceUponATime bot (default:"%include_ouat%")?
+@REM set /p include_automsg=Would you like to run AUTOMSG bot (default:"%include_automsg%")?
+@REM set /p include_sound=Should the bots run with AUDIO (default:"%include_sound%")?:
 
-set /p prompt_list_ouat=Enter the value for the yaml OnceUponATime prompt list (default:"%prompt_list_ouat%"):
-set /p prompt_list_automsg=Enter the value for the yaml AUTOMSG prompt list (default:"%prompt_list_automsg%"):
-set /p prompt_list_chatforme=Enter the value for the yaml CHATFORME prompt list (default:"%prompt_list_chatforme%"):  
+@REM set /p prompt_list_ouat=Enter the value for the yaml OnceUponATime prompt list (default:"%prompt_list_ouat%"):
+@REM set /p prompt_list_automsg=Enter the value for the yaml AUTOMSG prompt list (default:"%prompt_list_automsg%"):
+@REM set /p prompt_list_chatforme=Enter the value for the yaml CHATFORME prompt list (default:"%prompt_list_chatforme%"):  
 
-set /p input_port_number=What PORT NUMBER would you like to run the app on? (default:"%input_port_number%"):
+@REM set /p input_port_number=What PORT NUMBER would you like to run the app on? (default:"%input_port_number%"):
 
 :: Run Python command
 python "C:\_repos\chatforme_bots\twitch_bot.py" ^

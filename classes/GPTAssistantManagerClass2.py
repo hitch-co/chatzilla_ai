c07759429582dd46cb49037444365170293fcfa5
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2023-11-14 09:46:52 -0500

    workflow_() fix, logger, comments

--- classes/GPTAssistantManagerClass2.py
+++ classes/GPTAssistantManagerClass2.py
@@ -6,9 +6,11 @@ from my_modules import config
 from my_modules.my_logging import create_logger
 from my_modules.config import run_config
 
+debug_level = 'DEBUG'
+
 root_logger = create_logger(
     dirname='log', 
-    debug_level='INFO',
+    debug_level=debug_level,
     logger_name='root_GPTAssistantManager2',
     stream_logs=True
     )
@@ -48,8 +50,8 @@ class GPTClientAssistantManager:
     def __init__(self, config_data, gpt_client):
         self.logger = create_logger(
             dirname='log', 
-            debug_level='INFO',
-            logger_name='root_GPTClientAssistantManager',
+            debug_level=debug_level,
+            logger_name='GPTClientAssistantManager',
             stream_logs=True
             )
         self.config_data = config_data
@@ -74,7 +76,7 @@ class GPTClientAssistantManager:
         )
         self.assistants[assistant_name] = {'object':assistant, 'id':assistant.id}
 
-        self.logger.debug(f"This is the assistant:")
+        self.logger.debug(f"This is the assistant object for '{assistant_name}' with instructions: {assistant_instructions}")
         self.logger.debug(assistant)
         return assistant
     
@@ -83,8 +85,8 @@ class GPTThreadManager:
     def __init__(self, gpt_client):
         self.logger = create_logger(
             dirname='log', 
-            debug_level='INFO',
-            logger_name='root_GPTThreadManager',
+            debug_level=debug_level,
+            logger_name='GPTThreadManager',
             stream_logs=True
             )
         self.gpt_client = gpt_client
@@ -97,7 +99,7 @@ class GPTThreadManager:
         #store and link the thread
         self.threads[thread_name] = {'object':thread, 'id':thread.id}
 
-        self.logger.debug(f"This is the thread id/object:")
+        self.logger.debug(f"This is the thread id/object for {thread_name}:")
         self.logger.debug(thread.id)
         self.logger.debug(thread)
         return thread
@@ -106,15 +108,21 @@ class GPTThreadManager:
         # Returns the assistant associated with the given thread
         return self.thread_to_assistant.get(thread_name, None)
     
-    def add_message_to_thread(self, thread_id:str, role:str='user', message_content:str="Let the user know they should set the message_content variable"):
+    def add_message_to_thread(
+            self,
+            message_content, 
+            thread_id,
+            role='user'
+            ):
     
         message_object = self.gpt_client.beta.threads.messages.create(
             thread_id=thread_id,
             role=role,
             content=message_content
             )
-        self.logger.debug(f"message_object (type: {type(message_object)})")
+        self.logger.debug(f"message_object (thread_id: {thread_id}) (type: {type(message_object)})")
         self.logger.debug(message_object)
+        self.logger.debug(f"The content added to the thread: {message_content}")
         return message_object
 
 
@@ -122,11 +130,12 @@ class GPTAssistantResponseManager:
     def __init__(self, gpt_client):
         self.logger = create_logger(
             dirname='log', 
-            debug_level='INFO',
-            logger_name='root_GPTAssistantResponseManager',
+            debug_level=debug_level,
+            logger_name='GPTAssistantResponseManager',
             stream_logs=True
             )
         self.gpt_client = gpt_client
+        self.yaml_data = run_config()
 
     async def _get_response(self, thread_id, run_id, polling_seconds=1):
         while True:
@@ -135,6 +144,8 @@ class GPTAssistantResponseManager:
                 run_id=run_id
             )
             if response.status == 'completed':
+                self.logger.debug("This is the completed 'response' object")
+                self.logger.debug(response)
                 return response
             await asyncio.sleep(polling_seconds)
 
@@ -150,75 +161,52 @@ class GPTAssistantResponseManager:
             instructions=thread_instructions
         )
         await self._get_response(thread_id, run.id)
-        messages = self.gpt_client.beta.threads.messages.list(thread_id=thread_id)
+        response_thread_messages = self.gpt_client.beta.threads.messages.list(thread_id=thread_id)
         self.logger.debug("This is the 'messages' object:")
-        self.logger.debug(messages)
-        return messages
+        self.logger.debug(response_thread_messages)
+        return response_thread_messages
     
     def _extract_latest_response_from_thread_messages(self, response_thread_messages):
         # Sort the messages by 'created_at' in descending order
         sorted_response_thread_messages = sorted(response_thread_messages.data, key=lambda msg: msg.created_at, reverse=True)
+        self.logger.debug("This is the sorted_Response_thread_messages:")
+        self.logger.debug(sorted_response_thread_messages)
 
         # Find the most recent message with role 'assistant'
         for message in sorted_response_thread_messages:
             if message.role == 'assistant':
                 for content in message.content:
                     if content.type == 'text':
+                        self.logger.debug("This is the content.text.value")
+                        self.logger.debug(content.text.value)
                         return content.text.value
         return None
 
-    async def workflow(self, assistant_id, thread_id, thread_instructions):
+    async def workflow_gpt(self, assistant_id, thread_id, thread_instructions):
         # Get the response messages from the assistant
         response_thread_messages = await self._get_assistant_response_thread_messages(
             assistant_id=assistant_id,
             thread_id=thread_id,
             thread_instructions=thread_instructions
         )
-
+        self.logger.debug("This is the response_thread_messages object:")
+        self.logger.debug(response_thread_messages)
+        
         # Extract the latest response from the messages
         extracted_message = self._extract_latest_response_from_thread_messages(response_thread_messages)
-        
-        self.logger.info(f"This is the response from the assistant: '{extracted_message}'")
+
+        if len(extracted_message) > 400:
+            self.logger.debug("Message exceeded character length, running ")
+            response_thread_messages = await self._get_assistant_response_thread_messages(
+               assistant_id=assistant_id,
+                thread_id=thread_id,
+                thread_instructions=self.yaml_data['ouat_prompts']['shorten_response_length_prompt']
+            )
+            # Extract the latest response from the messages
+            extracted_message = self._extract_latest_response_from_thread_messages(response_thread_messages)
+
+        self.logger.info(f"This is the final response from workflow_gpt(): '{extracted_message}'")
         return extracted_message
-        
-# def main():
-#     # Configuration and API key setup
-#     yaml_data = run_config()
-#     openai.api_key = os.getenv('OPENAI_API_KEY')
-
-#     # Create client and manager instances
-#     gpt_client = openai.OpenAI()
-#     gpt_clast_mgr = GPTClientAssistantManager(config_data=yaml_data, gpt_client=gpt_client)
-#     gpt_thrd_mgr = GPTThreadManager(gpt_client=gpt_client)
-#     gpt_resp_mgr = GPTAssistantResponseManager(gpt_client=gpt_client)
-
-#     # Create assistant
-#     startstory_assistant = gpt_clast_mgr.create_assistant(
-#         assistant_name='startstory',
-#         assistant_instructions='you"re a bot with an attitude'
-#     )
-#     startstory_assistant_id = startstory_assistant.id
-
-#     # Create thread
-#     startstory_thread = gpt_thrd_mgr.create_thread(thread_name='startstory')
-#     startstory_thread_id = gpt_thrd_mgr.threads['startstory']['id']
-
-#     # Add message to thread
-#     gpt_thrd_mgr.add_message_to_thread(
-#         thread_id=startstory_thread_id, 
-#         role='user', 
-#         message_content="once upon a time..."
-#     )
-
-#     # Run workflow and retrieve response
-#     something = asyncio.run(gpt_resp_mgr.workflow(
-#         assistant_id=startstory_assistant_id,
-#         thread_id=startstory_thread_id,
-#         thread_instructions="finish the sentence in <20 words"
-#     ))
-
-#     # Example logging (if you have a logger setup)
-#     # root_logger.info(f"Workflow output: {something}")
 
 if __name__ == "__main__":
     # Configuration and API key setup

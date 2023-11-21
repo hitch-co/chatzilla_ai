54c08e992467eb3a108d01cc1261829a50b31329
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2023-11-21 11:43:31 -0500

    .bat runtime param update

--- twitch_bot_start.bat
+++ twitch_bot_start.bat
@@ -1,24 +1,20 @@
 @echo off
 
 :: Set default values
-set include_ouat=yes
-set include_automsg=no
-set include_sound=yes
 set prompt_list_ouat=newsarticle_dynamic
 set prompt_list_automsg=videogames
 set prompt_list_chatforme=standard
+
+set include_ouat=yes
+set include_automsg=no
+set include_sound=no
 set input_port_number=3000
 
-@REM :: Ask user to input param1
-@REM set /p include_ouat=Would you like to run OnceUponATime bot (default:"%include_ouat%")?
-@REM set /p include_automsg=Would you like to run AUTOMSG bot (default:"%include_automsg%")?
-@REM set /p include_sound=Should the bots run with AUDIO (default:"%include_sound%")?:
-
-@REM set /p prompt_list_ouat=Enter the value for the yaml OnceUponATime prompt list (default:"%prompt_list_ouat%"):
-@REM set /p prompt_list_automsg=Enter the value for the yaml AUTOMSG prompt list (default:"%prompt_list_automsg%"):
-@REM set /p prompt_list_chatforme=Enter the value for the yaml CHATFORME prompt list (default:"%prompt_list_chatforme%"):  
-
-@REM set /p input_port_number=What PORT NUMBER would you like to run the app on? (default:"%input_port_number%"):
+:: user to input
+set /p include_ouat=Would you like to include the STORYTELLER? (default:"%include_ouat%")?
+set /p include_automsg=Would you like to run AUTOMSG? (default:"%include_automsg%")?
+set /p include_sound=Should the bot run with AUDIO? (default:"%include_sound%")?:
+set /p input_port_number=What PORT NUMBER would you like to run the app on? (default:"%input_port_number%"):
 
 :: Run Python command
 python ".\twitch_bot.py" 
@@ -29,5 +25,4 @@ timeout /t 3
 :: Open a browser and go to the localhost page
 start http://localhost:%input_port_number%/auth
 
-:: Keep the window open
 pause

23edf64a46aaa6569a8764875c23ce8e4f01c2e2
ehitchman-dev <142246557+ehitchman-dev@users.noreply.github.com>
2024-01-21 13:01:08 -0500

    updated 'gpt_hello_world' to source from runtime args

--- twitch_bot_start.bat
+++ twitch_bot_start.bat
@@ -5,6 +5,7 @@ call "C:\_repos\chatzilla_ai_prod\chatzilla_ai\venv\Scripts\activate"
 
 :: Set default values
 set input_port_number=3000
+set gpt_hello_world=True
 set /p gpt_todo_prompt=What would you like to share about your tasklist on todays stream? (default:"Just plugging away, ask for details if you want to know more"):
 
 @REM :: user to input
